c52b3b049fb7be8cec9ce7e853a600a82c28102d
Grio Vino <iso2ez4vino@outlook.com>
2022-05-19 23:40:38 +0800

    improve bubble sort

--- Leetcode/DataStructure/Sort/sort/BubbleSort.go
+++ Leetcode/DataStructure/Sort/sort/BubbleSort.go
@@ -11,12 +11,29 @@ import (
 // BubbleSort 冒泡排序
 func BubbleSort() {
 	array := util.GenerateRandomArray(4, 0, 5)
-	len := len(array)
-	exchange := false
+	length := len(array)
 	fmt.Print("begin: ")
 	fmt.Println(array)
-	for i := 0; i < len; i++ {
-		for j := 0; j < len-1; j++ {
+	for i := 0; i < length; i++ {
+		for j := 0; j < length-1-i; j++ {
+			if array[j] > array[j+1] {
+				array[j], array[j+1] = array[j+1], array[j]
+			}
+		}
+	}
+	fmt.Print("end: ")
+	fmt.Println(array)
+}
+
+//优化点：定一个变量exchange记录当某一一轮是否发生交换行为，若为发生则判定已经排序成功，跳出循环即可。
+func BubbleSortV2() {
+	array := util.GenerateRandomArray(10, 0, 100)
+	len := len(array)
+	fmt.Print("begin: ")
+	fmt.Println(array)
+	for i := 0; i < len - 1; i++ {
+		exchange := false
+		for j := 0; j < len -1 - i; j++ {
 			if array[j] > array[j+1] {
 				array[j], array[j+1] = array[j+1], array[j]
 				exchange = true
@@ -28,6 +45,33 @@ func BubbleSort() {
 			break
 		}
 	}
+	fmt.Print("end: ")
+	fmt.Println(array)
+}
+
+//优化点：记录一轮交换后的最终索引，通过观察发现这个索引后的数字都是有序的，那么以后就不用比较这个索引后面的数字，即内层循环的边界是前面所说的最终索引。
+//当这个最终索引为0的时候（即前面没有数字的时候）排序就结束了。
+func BubbleSortV3() {
+	array := util.GenerateRandomArray(10, 0, 100)
+	n := len(array) - 1
+	fmt.Print("begin: ")
+	fmt.Println(array)
+	for(true) {
+		// 记录最后一次交换索引位置
+        last := 0;
+		for j := 0; j < n; j++ {
+			if array[j] > array[j+1] {
+				array[j], array[j+1] = array[j+1], array[j]
+				last = j;
+				fmt.Println(array)
+			}
+		}
+		n = last
+		if n == 0 {
+			break
+		}
+	}
+	fmt.Print("end: ")
 	fmt.Println(array)
 }
 
--- Leetcode/DataStructure/Sort/test.go
+++ Leetcode/DataStructure/Sort/test.go
@@ -1,29 +1,26 @@
 package main
 
 import (
-	"fmt"
-
 	"code.vino/GoStudy/Leetcode/DataStructure/Sort/sort"
-	"code.vino/GoStudy/Leetcode/DataStructure/Sort/util"
 )
 
 func main() {
 
-	fmt.Println(util.GenerateRandomArray(10, 0, 100))
+	// fmt.Println(util.GenerateRandomArray(10, 0, 100))
 
-	fmt.Println(util.GenerateNearlyOrderedArray(10, 2))
+	// fmt.Println(util.GenerateNearlyOrderedArray(10, 2))
 
-	s := []rune{1, 2, 3}
-	fmt.Sprintln(util.GenerateRandomString(10, s))
+	// s := []rune{1, 2, 3}
+	// fmt.Sprintln(util.GenerateRandomString(10, s))
 
-	fmt.Println(util.IsSorted(util.GenerateRandomArray(10, 0, 100)))
+	// fmt.Println(util.IsSorted(util.GenerateRandomArray(10, 0, 100)))
 
 	//Bubble Sort
-	// sort.BubbleSort()
+	sort.BubbleSortV3()
 
 	//SelectionSort
 	// sort.SelectionSort()
 
 	//InsertionSort
-	sort.InsertionSort()
+	// sort.InsertionSort()
 }
